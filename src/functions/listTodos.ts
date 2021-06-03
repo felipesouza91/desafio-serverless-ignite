ca07fd82bf6c4564fc71854b7ede713b432ca1d4
Felipe Souza <felipedb91@gmail.com>
2021-06-02 21:44:53 -0300

    feat: implements listtodos and savetodos functions

--- src/functions/listTodos.ts
+++ src/functions/listTodos.ts
@@ -1,11 +1,21 @@
-export const handle = async (event) => {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { document } from '../utils/dynamoClient';
+export const handle: APIGatewayProxyHandler = async (event) => {
+  const { user_id } = event.pathParameters;
+  const response = await document
+    .scan({
+      TableName: 'todos',
+      FilterExpression: 'user_id = :id',
+      ExpressionAttributeValues: {
+        ':id': user_id,
+      },
+    })
+    .promise();
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "is ok"
-    }),
+    body: JSON.stringify(response.Items),
     headers: {
-    "Content-Type": "application/json"
-    }
-  }
-}
+      'Content-Type': 'application/json',
+    },
+  };
+};
